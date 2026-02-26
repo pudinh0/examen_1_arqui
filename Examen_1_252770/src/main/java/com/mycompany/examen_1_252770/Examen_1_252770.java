76e71378fe9e133e25f4205aaa4684e534237ca5
Adel Mendez <adelmendez2@gmail.com>
2026-02-25 20:29:59 -0700

    x

---
+++ Examen_1_252770/src/main/java/com/mycompany/examen_1_252770/Examen_1_252770.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_1_252770;
+
+/**
+ *
+ * @author adell
+ */
+public class Examen_1_252770 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
