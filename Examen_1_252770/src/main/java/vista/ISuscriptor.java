1a353a270af67d2663d44080e5d9d60be445b78f
Adel Mendez <adelmendez2@gmail.com>
2026-02-27 14:00:22 -0700

    x

--- Examen_1_252770/src/main/java/vista/ISuscriptor.java
+++ Examen_1_252770/src/main/java/vista/ISuscriptor.java
@@ -7,9 +7,13 @@ package vista;
 import modelo.IModeloVista;
 
 /**
- *
+ * Interfaz que contiene el metodo update siendo implementado por FrameConstanciaPrincipal
  * @author adell
  */
 public interface ISuscriptor {
+    /**
+     * metodo que actualizara la vista por medio de modeloVista
+     * @param modeloVista instancia que tendra los getters para actualizar la vista
+     */
     public void update(IModeloVista modeloVista);
 }
